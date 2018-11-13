a31148769a18f49ca82a12d156ce56a78627dd52
Tiago Cordeiro <tiago@mulhergorila.com>
2018-11-13 11:10:47 -0200

    Configura testes básicos para website e beer

--- beer/tests.py
+++ beer/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User, Group
+from django.test import RequestFactory, TestCase
+from . import views
 
-# Create your tests here.
+
+class BeerViewsTestes(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.group = Group.objects.create(name='Testes')
+        self.group.user_set.add(self.user)
+
+    def test_beer_list_anonimo(self):
+        request = self.factory.get('')
+        request.user = AnonymousUser()
+
+        response = views.beer_list(request)
+        self.assertEqual(response.status_code, 200)
--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User, Group
+from django.test import RequestFactory, TestCase
+from . import views
 
-# Create your tests here.
+
+class WebsiteViewsTestes(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.group = Group.objects.create(name='Testes')
+        self.group.user_set.add(self.user)
+
+    def test_index_anonimo(self):
+        request = self.factory.get('')
+        request.user = AnonymousUser()
+
+        response = views.index(request)
+        self.assertEqual(response.status_code, 200)
